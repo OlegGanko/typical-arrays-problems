f34428d015433aaeafa9fa31c0bd50622ac12c38
olegganko <olegganko@gmail.com>
2020-09-30 18:05:07 +0300

    init: start task  typical-arrays-problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,44 @@
 
 exports.min = function min (array) {
-  return 0;
+ if (array === undefined || array.length === 0) return 0;
+  
+  let arrayMin = [],
+  i = 0;
+    while (i < array.length) {
+      if (typeof array[i] === "number") {
+        arrayMin.push(array[i]);
+      }
+      i++;
+    }
+    return Math.min(...arrayMin);
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (array === undefined || array.length === 0) return 0;
+  
+    let arrayMin = [],
+    i = 0;
+    while (i < array.length) {
+      if (typeof array[i] === "number") {
+        arrayMin.push(array[i]);
+      }
+      i++;
+    }
+    return Math.max(...arrayMin);
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (array === undefined || array.length === 0) return 0;
+
+    let i = 0,
+    sumNumber = 0,
+    n = 0;
+    while (i < array.length) {
+      if (typeof array[i] === "number") {
+        sumNumber += array[i];
+        n++;
+      }
+      i++;
+    }
+    return sumNumber / n;
 }
